2a08f6577ad8081d0316ad92de06d648e618dbce
Kyriakos Ziakoulis <cooxlee@gmail.com>
2018-10-28 18:32:59 +0200

    Updates

---
+++ precache-manifest.f0598d2c6a59ef11175850efb0a1277c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29a6d96011195bd09f8bca7a2ed6251b",
+    "url": "/rick-and-morty/static/media/logo.29a6d960.svg"
+  },
+  {
+    "revision": "e8a6392fe1cd7f5726ac",
+    "url": "/rick-and-morty/static/js/runtime~main.e8a6392f.js"
+  },
+  {
+    "revision": "c6c4f6dfd43c5eed5657",
+    "url": "/rick-and-morty/static/js/main.c6c4f6df.chunk.js"
+  },
+  {
+    "revision": "72975b2b421cbc9422ba",
+    "url": "/rick-and-morty/static/js/1.72975b2b.chunk.js"
+  },
+  {
+    "revision": "c6c4f6dfd43c5eed5657",
+    "url": "/rick-and-morty/static/css/main.15060811.chunk.css"
+  },
+  {
+    "revision": "5754965b936d966fb8e41884db3a6a8f",
+    "url": "/rick-and-morty/index.html"
+  }
+];
